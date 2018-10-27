ecdc9ac1506ef39b5c393915ccf5b9786a2d3d87
Luiz Carlos Silva de Araujo <lcsdafl.eng16@uea.edu.br>
2018-10-27 12:29:53 -0400

    update com comentários

--- leitor-escritor.c
+++ leitor-escritor.c
@@ -4,60 +4,65 @@
 #include <pthread.h>
 #include <time.h>
 
-int Buffer_Size = 10;
 sem_t mutex, escritex;
-int ret0, ret1, readerCount = 0, READERS = 2;
-pthread_t leitores[2], escritor;
-//srand(time(NULL));
+int ret0, ret1, readerCount = 0
+pthread_t leitores[2], escritores[2];//Quantidade arbitrária
+                                     //de leitores e escritores
+
     
 void reader(){
     int i = 0;
-    while(i < 100){ //while(1) para emular uma execuï¿½ï¿½o sem limite
-        sem_wait(&mutex);
+    while(i < 100){ //while(1) para emular uma execucao sem limite
+        sem_wait(&mutex); //bloqueia o acesso ao readerCount por outras threads
         readerCount++;
-        printf("Há %d leitores\n", readerCount);
-        if(readerCount == 1) sem_wait(&escritex);
+        printf("Há %d leitores\n", readerCount); //mostra quantos leitores estão na fila
+        if(readerCount == 1) sem_wait(&escritex); //verifica se há alguma thread lendo para bloquear a escrita
         sem_post(&mutex);
         
-        printf("Lendo...\n");
-        sleep(1+ (rand()%3));
+        printf("Lendo...\n");//abstração de uma leitura
+        sleep(1+ (rand()%3));//para randomizar um pouco o comportamento
         
-        sem_wait(&mutex);
+        sem_wait(&mutex);//após um leitor acabar
         readerCount--;
         
-        if(readerCount == 0) sem_post(&escritex);
-        sem_post(&mutex);
+        if(readerCount == 0) sem_post(&escritex);//se for o ultimo, desbloqueia a escrita
+        sem_post(&mutex);//libera acesso para readerCount
         
-        i++;
+        i++;//somente para o loop acabar
     }
 }
 
 void writer(){
     int i = 0;
     while(i < 100){ //while(1) para emular uma execuï¿½ï¿½o sem limite
-        sem_wait(&escritex);
-        printf("Escrevendo...\n");
-        sem_post(&escritex);
-        sleep(3+(rand()%5));
-        
-        i++;
+        sem_wait(&escritex);//bloqueia a escrita para que haja somente um escritor por vez
+        printf("Escrevendo...\n");//abstração de uma escrita
+        sleep(3+(rand()%5));//randomizar o comportamento um pouco
+
+        sem_post(&escritex);//libera a escrita
+
+        i++;//somente para o loop acabar
     }
 }
 
 int main(void) {
     
-    sem_init(&mutex, 0, 1);
-    sem_init(&escritex, 0, 1);
+    sem_init(&mutex, 0, 1); //cria o semaforo para o reader count
+    sem_init(&escritex, 0, 1);//cria o semaforo para o escritor
     
-    for(int i=0; i<READERS; i++){
-    	ret0 = pthread_create( &leitores[i], NULL,reader, NULL);
+    for(int i=0; i<2; i++){
+    	ret0 = pthread_create( &leitores[i], NULL,reader, NULL);//cria uma thread para cada leitor
 	}
-    ret1 = pthread_create(&escritor, NULL, writer, NULL);
-    
-    for(int i=0; i<READERS; i++){
-    	pthread_join(leitores[i], NULL);
+    for(int i = 0; i<2; i++){
+        ret1 = pthread_create(&escritores[i], NULL, writer, NULL);//cria uma thread para cada escritor
+    }
+    for(int i=0; i<2; i++){
+    	pthread_join(leitores[i], NULL);//para que o main espere a execuação das threads
 	}
-    pthread_join(escritor, NULL);
+    for(int i = 0; i<2; i++){
+        pthread_join(escritores[i], NULL);//para que o main espere a execução das threads
+    }
+
     
 	return 0;
 }
--- prodcons.c
+++ prodcons.c
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <time.h>
 
-int Buffer_Size = 10;
+int Buffer_Size = 10;//tamanho arbitrário para o buffer
 sem_t mutex, empty, full;
 int ret0, ret1, count = 0;
 pthread_t produtor, consumidor;
@@ -12,42 +12,43 @@ pthread_t produtor, consumidor;
 void produzir(){
     int i = 0;
     while(i < 100){ //while(1) para emular uma execução sem limite
-        sem_wait(&empty);
-        sem_wait(&mutex);
-        printf("Produziu na posicao %d \n", count);
-        count++;
-        sem_post(&mutex);
-        sem_post(&full);
-        sleep(2);
-        i++;
+        sem_wait(&empty); //inicialmente com o tamanho do buffer, verifica os espaços vazios
+        sem_wait(&mutex);//controle da região crítica
+        count++;//quantidade de itens e posição para inserção de itens no buffer
+        printf("Produziu na posicao %d \n", count);//abstração do produtor
+        sleep(2);//para simular o tempo de produção
+        sem_post(&mutex);//saida da região crítica
+        sem_post(&full);//inicialmente com tamanho 0, agora é incrementado, representando a adição de um item ao buffer
+
+        i++;//apenar para terminar o loop
     }
 }
 
 void consumir(){
     int i = 0;
     while(i < 100){ //while(1) para emular uma execução sem limite
-        sem_wait(&full);
-        sem_wait(&mutex);
-        printf("Consumiu na posicao %d \n", count);
-        count--;
-        sem_post(&mutex);
-        sem_post(&empty);
-        sleep(4);
-        i++;
+        sem_wait(&full);//verifica se o tamanho do buffer ainda é 0
+        sem_wait(&mutex);//controle da região crítica
+        count--;//remove um item do buffer, liberando um espaço
+        printf("Consumiu na posicao %d \n", count); //abstração de um consumo
+        sleep(4);//simular tempo de consumo
+        sem_post(&mutex);//saída da regiçao crítica
+        sem_post(&empty);//incrementa o empty, representando a remoção de um item do buffer
+        i++;//apenas para terminar o loop
     }
 }
 
 int main(void) {
     
-    sem_init(&mutex, 0, 1);
-    sem_init(&empty, 0, Buffer_Size);
-    sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);//criação do semáforo para região crítica
+    sem_init(&empty, 0, Buffer_Size);//criação do semáforo para verificar se é possível adicionar itens ao buffer
+    sem_init(&full, 0, 0);//criação do semáforo para verificar se é possível remover itens do buffer
     
-    ret0 = pthread_create(&produtor, NULL, produzir, NULL);
-    ret1 = pthread_create(&consumidor, NULL, consumir, NULL);
+    ret0 = pthread_create(&produtor, NULL, produzir, NULL);//cria a thread para um produtor
+    ret1 = pthread_create(&consumidor, NULL, consumir, NULL);//cria a threa para um consumidor
     
-    pthread_join(produtor, NULL);
-    pthread_join(consumidor, NULL);
+    pthread_join(produtor, NULL);//faz o main esperar a execução da thread do produtor
+    pthread_join(consumidor, NULL);//faz o main esperar a execução da thread do produtor
     
 	return 0;
 }
